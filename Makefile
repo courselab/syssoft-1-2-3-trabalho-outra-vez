719321b8348fe8f84a65e1b2bbcf30bd5be86787
Pones <felipemomma@usp.br>
2025-06-03 17:41:55 -0300

    Desenvolvimento
    Feito o exercício da memoria
    
    - Implementado no libc.S
    - Alterado o Makefile
    - Implementado na main.c

---
+++ Makefile
@@ -0,0 +1,36 @@
+#    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+#    SPDX-FileCopyrightText: 2025 Pones <felipemoma@yahoo.com.br>
+#   
+#    SPDX-License-Identifier: GPL-3.0-or-later
+#
+#  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+#  and includes modifications made by the following author(s):
+#  Pones <felipemoma@yahoo.com.br>
+
+CC=gcc
+MAKE=make
+
+main.o : main.c
+	gcc -m16 -O1 -Wall -fno-pic -fno-asynchronous-unwind-tables -c $< -fno-builtin -nostdinc -I. -o $@ 
+
+libc.o : libc.S
+	gcc -m16 -O1 -Wall -fno-pic -fno-asynchronous-unwind-tables -c $< -fno-builtin -o $@
+
+crt0.o : crt0.S
+	gcc -m16 -O1 -Wall -fno-pic -fno-asynchronous-unwind-tables -c $< -fno-builtin -o $@
+
+hello.bin : main.o libc.o hello.ld crt0.o
+	gcc -m16 main.o libc.o -nostartfiles -nostdlib -L. -T hello.ld \
+	-orphan-handling=discard -o hello.bin
+
+
+
+#
+# Housekeeping
+#
+
+clean:
+	rm -f  *.bin *.elf *.o *.s *.iso *.img *.i kh
+
+# SYSeg's  convenience rules (not related to the example itself)
+include bintools.mk
---
+++ main.c
@@ -0,0 +1,50 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2025 Pones <felipemoma@yahoo.com.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  Pones <felipemoma@yahoo.com.br>*/
+
+/* main.c */
+#include <stdio.h>
+
+char buffer[6];
+
+int main(void) {
+    unsigned short tamanho = memsize();   /* chama INT 0x12 */
+    /* converte tamanho para string decimal */
+    /* empilha digitos no final de buffer e inverte */
+    if (tamanho == 0) {
+        buffer[0] = '0';
+        buffer[1] = '\0';
+    } else {
+        unsigned short tmp = tamanho;
+        int idx = 0;
+        char rev[5];
+        while (tmp) {
+            rev[idx++] = '0' + (tmp % 10);
+            tmp /= 10;
+        }
+        /* inverte rev para buffer */
+        for (int i = 0; i < idx; i++) {
+            buffer[i] = rev[idx - 1 - i];
+        }
+        buffer[idx] = '\0';
+    }
+
+    /* imprimindo resultados */
+    printf("Hello World\n");
+    printf("Mem: ");
+    printf(buffer);
+    printf(" KB\n");
+
+    for(;;){}
+
+    return 0;
+}
+
+
+
