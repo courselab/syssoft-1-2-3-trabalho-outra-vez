bf9e1145539ad6a33d0cd830d35fd3a9a6e93ace
reisvictor1 <reisvictor448@gmail.com>
2025-07-05 10:44:11 -0300

    add quiz

---
+++ quiz/p1.c
@@ -0,0 +1,19 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2025 reisvictor1 <reisvictor448@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  reisvictor1 <reisvictor448@gmail.com>
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  printf ("Main's ddress: %p\n", main);
+  
+  return 0;
+}
